35b8da5f814a7cce81fc3c6df5f8fd6c90005214
iKun <59053257+ikun1998802@users.noreply.github.com>
2026-09-02 00:40:39 +0800

    Update auto.js

--- auto.js
+++ auto.js
@@ -8,18 +8,18 @@
     function checkLuckyBag() {
         LuckyBagTimer = setInterval(() => {
             const dom = document.querySelector(LuckyBagClassName)
-            console.log("开始检测福袋。。。")
+            console.log("auto.js 开始检测福袋。。。")
             if (dom) {
                 const m = dom.innerText.split(":")[0]
-                console.log("当前分钟：", m)
+                console.log("auto.js 当前分钟：", m)
                 if (dom && m == '02') {
-                    console.log("时间到已点击福袋")
+                    console.log("auto.js 时间到已点击福袋")
                     dom.click()
                     clearInterval(LuckyBagTimer)
                     setTimeout(() => clickSendMsg(), 1000)
                 }
             } else {
-                console.log("没有福袋。。。")
+                console.log("auto.js 没有福袋。。。")
             }
         }, 1000)
     }
@@ -27,14 +27,14 @@
     function checkResult() {
         ResultTimer = setInterval(() => {
             const dom = document.querySelector(ResultClassName)
-            console.log("检测结果弹窗。。。");
+            console.log("auto.js 检测结果弹窗。。。");
             if (dom) {
-                console.log("检测到结果弹窗已关闭");
+                console.log("auto.js 检测到结果弹窗已关闭");
                 dom.click()
                 clearInterval(ResultTimer)
                 setTimeout(() => checkLuckyBag(), 1000)
             } else {
-                console.log("结果还没出来");
+                console.log("auto.js 结果还没出来");
             }
         }, 1000)
     }
@@ -42,7 +42,7 @@
     function clickSendMsg() {
         const dom = document.querySelector(SendMsgClassName)
         if (dom && dom?.innerText == '一键发评论参与福袋') {
-            console.log("抽奖弹窗已打开，已发送消息");
+            console.log("auto.js 抽奖弹窗已打开，已发送消息");
             dom.click()
             checkResult()
         }
